57df90240bb8afe1e94999ec25ee8b005c39b287
elma bt <elmabristric20@gmail.com>
2021-02-24 11:38:11 +0100

    player class

--- Card21/Program.cs
+++ Card21/Program.cs
@@ -1,9 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
 
 namespace Card21
 {
+    
     class Program
     {
+        class player
+        {
+         public string = "player1"
+
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Player!");
